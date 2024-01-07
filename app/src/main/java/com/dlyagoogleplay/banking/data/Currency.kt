42ec081a00428022d401aee37c9da47571c71048
Andrey Ostapovich <andreyostapovich958@gmail.com>
2024-01-07 23:54:03 +0300

    first

---
+++ app/src/main/java/com/dlyagoogleplay/banking/data/Currency.kt
@@ -0,0 +1,10 @@
+package com.dlyagoogleplay.banking.data
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class Currency (
+    val name: String,
+            val buy: Float,
+                    val sell: Float,
+                            val icon: ImageVector
+)
